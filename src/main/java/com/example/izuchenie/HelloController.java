a5a18dda964cd8b105578a2b393d5b9111079b89
ruslanzverkov <createmed@mail.ru>
2022-09-28 23:16:56 +0300

    1

--- src/main/java/com/example/izuchenie/HelloController.java
+++ src/main/java/com/example/izuchenie/HelloController.java
@@ -1,7 +1,18 @@
 package com.example.izuchenie;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+   @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping(path="/Hello")
+    public String answerHello(@RequestParam("name")String userName) {
+        return "Hello";
+    }
 }
