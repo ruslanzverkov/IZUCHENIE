d4d23791c7072a7f6742af4c5fb3d8cbe5b9282c
ruslanzverkov <createmed@mail.ru>
2022-10-12 21:04:54 +0300

    2.4 polimorfism

--- src/main/java/com/example/izuchenie/domain/Driver.java
+++ src/main/java/com/example/izuchenie/domain/Driver.java
@@ -1,4 +1,19 @@
 package com.example.izuchenie.domain;
 
-public class Driver {
+public class Driver extends Person{
+    private  String driverLicenseNumber;
+
+    public Driver(String name, String surName, String passport, String driverLicenseNumber) {
+        super(name, surName, passport);
+        this.driverLicenseNumber = driverLicenseNumber;
+
+    }
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
 }
--- src/main/java/com/example/izuchenie/domain/Person.java
+++ src/main/java/com/example/izuchenie/domain/Person.java
@@ -1,4 +1,40 @@
 package com.example.izuchenie.domain;
 
 public class Person {
+
+    private String name;
+    private String surName;
+    private String passport;
+
+    public Person(String name, String surName, String passport) {
+        this.name = name;
+        this.surName = surName;
+        this.passport = passport;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void changePassportNumber(String passport) {
+        if (passport != null && !passport.isBlank()) {
+            this.passport=passport;
+        }
+    }
 }
